1f0a42769b581fac75718f2e5490aebf50e8f43c
esganac <31550311+esganac@users.noreply.github.com>
2017-09-22 14:54:01 -0700

    Add files via upload

---
+++ Caitlin Esgana Quiz #4.R
@@ -0,0 +1,24 @@
+#### Caitlin Esgana
+#### Quiz 4
+#### September 22, 2017
+
+# 1
+curve(3*(x^3)+(x^2)-3,main="Cool Title",from= -5,to= 5)
+
+# 1a
+curve((20*x)+7,add=T,col= "red")
+
+# 1b
+
+
+# 2
+x.matrix<-matrix(c(4,5,3,8),nrow=2)
+
+# 2a
+x2.matrix<- x.matrix^2
+
+# 2b
+solve(x.matrix)
+
+# 2c
+x.matrix %*% solve(x.matrix)
