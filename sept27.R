528f1487353ddb6802c4a9d3cd4f39d17f4fbc14
esganac <31550311+esganac@users.noreply.github.com>
2017-09-27 14:48:14 -0700

    Add files via upload

---
+++ sept27.R
@@ -0,0 +1,42 @@
+mylist<- list(myvec= c(4,7,8),mymatrix= matrix(1:4,nrow= 2))
+mylist
+# pulls out the vector
+mylist[[1]]
+
+# numeric
+class(mylist[[1]])
+# list
+class(mylist[1])
+
+# function
+areaofrectangle<- function(length,width) {
+  area= length*width
+  return(area)
+}
+areaofrectangle(3,5)
+
+## Exercises ##
+
+# 2
+x<- c(5,2,17,300,9,22)
+# 2a
+sq.x<- NULL
+for(i in 1:length(x)){
+  sq.x<- c(sq.x,x[i]^2)
+}
+sq.x
+
+# 2b
+product.x<- NULL
+for(i in 1:length(x)-1){
+  product.x<- c(product.x,x[i]*x[i+1])
+}
+sum(product.x)
+
+# 2c
+ten.x<- NULL
+for (i in 1:length(x)){
+  if(x[i]>10){
+    ten.x<- c(ten.x,x[i])
+  }
+}
